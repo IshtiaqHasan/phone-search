0c31355513d7f273bacaca550f3a38641bcc5624
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-03-01 00:36:50 +0600

    phone details added

--- js/api.js
+++ js/api.js
@@ -11,6 +11,7 @@ const searchPhones = () => {
 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     data.forEach(phone => {
         //console.log(phone);
         const div = document.createElement('div');
@@ -21,10 +22,54 @@ const displaySearchResult = data => {
             <div class="card-body">
                 <h5 class="card-title">Brand: ${phone.brand}</h5>
                 <h5 class="card-title">Model-Name: ${phone.phone_name}</h5>
-                <button type="button" class="btn btn-primary btn-lg">Details</button>
+                <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary btn-lg">Details</button>
             </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="shadow bg-body rounded-3">
+        <img src="${phone.image}" class="card-img-top w-25 " alt="...">
+        <div class="card-body ">
+            <h5 class="card-title">Name: ${phone.name}</h5>
+            <h5 class="card-title">
+            Specification: 
+            </h5>
+            <h6 class="card-title">
+            Chipset:  ${phone.mainFeatures.chipSet}
+            </h6>
+            <h6 class="card-title">
+            Display Size:  ${phone.mainFeatures.displaySize}
+            </h6>
+            <h6 class="card-title">
+            Memory:  ${phone.mainFeatures.memory}
+            </h6>
+            <h6 class="card-title">
+            Storage:  ${phone.mainFeatures.storage}
+            </h6>
+            <h6 class="card-title">
+            Sensors:  ${phone.mainFeatures.sensors}
+            </h6>
+            <p class="card-text">Realese Date: ${phone.releaseDate}</p>
+        </div>
+    </div>
+    
+    `;
+    phoneDetails.appendChild(div)
 }
