980dd68534e8ca735aefaa9a00535c427920a411
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-03-01 11:06:29 +0600

    search-error messages included

--- js/api.js
+++ js/api.js
@@ -3,31 +3,45 @@ const searchPhones = () => {
     const searchText = searchField.value;
     //console.log(searchText);
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data))
+    if (searchText == '') {
+        document.getElementById("phoneSearch-error").style.display = "block";
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.data))
+        document.getElementById("phoneSearch-error").style.display = '';
+    }
+
 }
 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    data.forEach(phone => {
-        //console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="shadow bg-body rounded-3">
-            <img src="${phone.image}" class="card-img-top w-50" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Brand: ${phone.brand}</h5>
-                <h5 class="card-title">Model-Name: ${phone.phone_name}</h5>
-                <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary btn-lg">Details</button>
+    if (data == 0) {
+        document.getElementById('phoneNot-found').style.display = 'block';
+    }
+    else {
+        data.forEach(phone => {
+            //console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="shadow bg-body rounded-3">
+                <img src="${phone.image}" class="card-img-top w-50" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Brand: ${phone.brand}</h5>
+                    <h5 class="card-title">Model-Name: ${phone.phone_name}</h5>
+                    <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary btn-lg">Details</button>
+                </div>
             </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    })
+            `;
+            searchResult.appendChild(div);
+            document.getElementById('phoneNot-found').style.display = '';
+        })
+    }
+
 }
 
 const loadPhoneDetail = phoneId => {
@@ -42,32 +56,35 @@ const displayPhoneDetail = phone => {
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
     const div = document.createElement('div')
-    div.classList.add('card');
+    div.classList.add('col');
     div.innerHTML = `
-        <img src="${phone.image}" class="card-img-top w-25 mx-auto" alt="...">
-        <div class="card-body mx-auto">
-            <h5 class="card-title">Name: ${phone.name}</h5>
-            <h5 class="card-title">
-            Specification: 
-            </h5>
-            <h6 class="card-title">
-            Chipset:  ${phone.mainFeatures.chipSet}
-            </h6>
-            <h6 class="card-title">
-            Display Size:  ${phone.mainFeatures.displaySize}
-            </h6>
-            <h6 class="card-title">
-            Memory:  ${phone.mainFeatures.memory}
-            </h6>
-            <h6 class="card-title">
-            Storage:  ${phone.mainFeatures.storage}
-            </h6>
-            <h6 class="card-title">
-            Sensors:  ${phone.mainFeatures.sensors}
-            </h6>
-            <p class="card-text">Realese Date: ${phone.releaseDate}</p>
+        <div id="phone-details" class="shadow bg-body rounded-3">
+           <img src="${phone.image}" class="card-img-top w-25" alt="...">
+           <div class="card-body ps-5">
+                <h5 class="card-title">Name: ${phone.name}</h5>
+                <h5 class="card-title">Brand: ${phone.brand}</h5>
+                <h5 class="card-title">
+                Specification: 
+                </h5>
+                <h6 class="card-title">
+                Chipset-  ${phone.mainFeatures.chipSet}
+                </h6>
+                <h6 class="card-title">
+                Display Size-  ${phone.mainFeatures.displaySize}
+                </h6>
+                <h6 class="card-title">
+                Memory-  ${phone.mainFeatures.memory}
+                </h6>
+                <h6 class="card-title">
+                Storage-  ${phone.mainFeatures.storage}
+                </h6>
+                <h6 class="card-title">
+                Sensors-  ${phone.mainFeatures.sensors}
+                </h6>
+                <h5 class="card-text">Realese Date: ${phone.releaseDate}</h5>
+           </div>
         </div>
-    
+        
     
     `;
     phoneDetails.appendChild(div)
